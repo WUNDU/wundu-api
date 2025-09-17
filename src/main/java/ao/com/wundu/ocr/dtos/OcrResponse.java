5651a3b972f11e8c95ddeda35f5f6db749661490
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2025-09-17 20:30:37 +0100

    :lock: fix(OCR): removendo token exposto em comentário

--- src/main/java/ao/com/wundu/ocr/dtos/OcrResponse.java
+++ src/main/java/ao/com/wundu/ocr/dtos/OcrResponse.java
@@ -2,7 +2,6 @@ package ao.com.wundu.ocr.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record OcrResponse(
